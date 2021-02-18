fa279bb3da2fc0e8a8abbe5a906e262af83afab1
Ji-Yong <wldyd2356@gmail.com>
2021-02-18 23:15:14 +0900

    feat: 라우터 적용

--- news-viewer-repeat/src/App.js
+++ news-viewer-repeat/src/App.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-  return (
-    <div>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage}></Route>;
 };
 
 export default App;

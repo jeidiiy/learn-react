941953da39a46970ea09361d537040dc19ae8df8
Ji-Yong <wldyd2356@gmail.com>
2021-02-18 22:32:53 +0900

    feat: 기본 컴포넌트 구현

--- news-viewer-repeat/src/App.js
+++ news-viewer-repeat/src/App.js
@@ -1,27 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 const App = () => {
-  const [data, setData] = useState(null);
-
-  const onClick = useCallback(async () => {
-    try {
-      const res = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=81e7f740025843d8b8f7993b10f14919'
-      );
-      setData(res.data);
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
-
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-      )}
+      <NewsList />
     </div>
   );
 };

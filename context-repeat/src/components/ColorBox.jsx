3b271eb7c6f14e5bbd89dc85d30988f1741ead1f
Ji-Yong <wldyd2356@gmail.com>
2021-02-20 23:20:25 +0900

    feat: Context API를 Hook으로 사용

--- context-repeat/src/components/ColorBox.jsx
+++ context-repeat/src/components/ColorBox.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ColorContext from '../contexts/color';
-import { ColorConsumer } from '../contexts/color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: '64px',
-              height: '64px',
-              background: state.color,
-            }}
-          ></div>
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subcolor,
-            }}
-          ></div>
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color,
+        }}
+      ></div>
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor,
+        }}
+      ></div>
+    </>
   );
 };
 

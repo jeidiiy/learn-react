78eb4c37d34171b12edc71df514d5e421b8a3a85
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-03-09 22:00:39 +0900

    feat: realized counter

---
+++ saga-ex/src/containers/CounterContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+import Counter from '../components/Counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+const mapStateToProps = state => ({ number: state.counter.number });
+
+const mapDispatchToProps = dispatch => ({
+  increase: () => dispatch(increase()),
+  decrease: () => dispatch(decrease()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
